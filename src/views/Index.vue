fb0a31544e3be2f916f594d1be2b0a8a6fb0709f
kitsunecat <scooping.fox@gmail.com>
2019-05-20 17:07:14 +0900

    [add] index to osyacos

---
+++ src/views/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <table>
+    <thead>
+      <th>なまへ</th>
+      <th>Github URL</th>
+      <th>Comment</th>
+    </thead>
+    <tbody>
+      <tr  v-for="osyaco in osyacos" :key="osyaco.id">
+        <td>{{osyaco.name}}</td>
+        <td>{{osyaco.giturl}}</td>
+        <td>{{osyaco.comment}}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "Index",
+  data() {
+    return {
+      osyacos: [] };
+  },
+  mounted () {
+    axios
+      .get('https://vizzduwbk3.execute-api.ap-northeast-1.amazonaws.com/dev/todos')
+      .then(response => (this.osyacos = response.data))
+  }
+};
+</script>
